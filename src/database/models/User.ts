da09f1e0f5fc559a1d4eb455753b8c573a49cfdb
Kaua2123 <kauantnetsoares@gmail.com>
2024-02-15 09:30:02 -0300

    primeira model criada

---
+++ src/database/models/User.ts
@@ -0,0 +1,51 @@
+import sequelize from 'sequelize';
+import db from '.';
+import { Model } from 'sequelize';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare email: string;
+  declare password_hash: string;
+  declare created_at: Date;
+  declare updated_at: Date;
+}
+
+User.init(
+  {
+    id: {
+      type: sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password_hash: {
+      type: sequelize.STRING,
+      allowNull: false,
+    },
+    created_at: {
+      type: sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: sequelize.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    tableName: 'user',
+  },
+);
+
+export default User;
