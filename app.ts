ec75511da9b75e45ad9bb8745fdc03ce1023f9d8
Kaua2123 <kauantnetsoares@gmail.com>
2024-02-16 09:33:55 -0300

    configurações

--- app.ts
+++ app.ts
@@ -1,5 +1,12 @@
+import cors from 'cors';
 import express from 'express';
+import path from 'path';
 
 const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'uploads')));
 
 export default app;
