cefc6ce38e5f44825c06358973adb1942a9ee6c6
Kaua2123 <kauantnetsoares@gmail.com>
2024-02-16 08:04:51 -0300

    migration de comments

---
+++ src/database/migrations/20240216110039-comments.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    queryInterface.createTable('comments', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+      post_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'posts',
+          key: 'id',
+        },
+      },
+      content: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  // async down (queryInterface, Sequelize) {}
+};
