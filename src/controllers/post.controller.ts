596769f524c9a756ff30eddc6fcc02c6731aff28
Kaua2123 <kauantnetsoares@gmail.com>
2024-02-17 10:51:47 -0300

    delete e update de posts

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -5,7 +5,7 @@ class PostController {
   async index(req: Request, res: Response, next: NextFunction) {
     try {
       const posts = await Post.findAll();
-      console.log(posts);
+
       return res.status(200).json(posts);
     } catch (error) {
       next(error);
@@ -15,7 +15,9 @@ class PostController {
   async show(req: Request, res: Response, next: NextFunction) {
     try {
       const { id } = req.params;
+
       const post = await Post.findByPk(id);
+
       return res.status(200).json(post);
     } catch (error) {
       next(error);
@@ -27,6 +29,32 @@ class PostController {
       const createdPost = await Post.create(req.body);
 
       return res.status(200).json(createdPost);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+
+      const post = await Post.findByPk(id);
+      const updatedPost = await post?.update(req.body);
+
+      return res.status(200).json(updatedPost);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+
+      const post = await Post.findByPk(id);
+      await post?.destroy();
+
+      return res.status(200).json(null);
     } catch (error) {
       next(error);
     }
--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -6,5 +6,7 @@ const postRouter = Router();
 postRouter.get('/post', PostController.index);
 postRouter.get('/post/:id', PostController.show);
 postRouter.post('/post/create', PostController.store);
+postRouter.put('/post/update/:id', PostController.update);
+postRouter.delete('/post/delete/:id', PostController.delete);
 
 export default postRouter;
